1f8638909b788e80fe91c31f96089ae51f01eb1b
bryndav <d.bryngelsson@gmail.com>
2016-10-11 20:16:28 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,18 +5,29 @@ import shutil
 
 PATH = '/home/dbry/ownCloud/Foton/Mobilbilder'
 
-for entry in os.listdir(PATH):
-    if entry.endswith('.jpg') or entry.endswith('.JPG'):
-        
-        source = PATH + '/' + entry
-        
-        img = Image.open(source)
-        exif = {
-            PIL.ExifTags.TAGS[k]: v
-            for k, v in img._getexif().items()
-            if k in PIL.ExifTags.TAGS
+def generateExif(filepath):
+    """
+    A function for genereating information about a image header.
+    """
+    img = Image.open(filepath)
+    exif = {
+        PIL.ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if k in PIL.ExifTags.TAGS
         }
-        
-        destination = PATH + '/' + exif['DateTime'][:-12].replace(':', '-')
-        if not os.path.isdir(destination): os.makedirs(destination)
-        shutil.move(source, destination)
+    return exif
+
+if __name__ == '__main__':
+
+    for entry in os.listdir(PATH):
+  
+        if entry.endswith('.jpg') or entry.endswith('.JPG'):
+            filepath = PATH + '/' + entry
+            currentExif = generateExif(filepath)
+
+            destination = PATH + '/' + currentExif['DateTime'][:-12].replace(':', '-')
+
+            if not os.path.isdir(destination): os.makedirs(destination)
+            try:
+                shutil.move(filepath, destination)
+            except shutil.Error: pass
