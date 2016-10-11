82f1b1e63c01768d847c073bc6e368e0e5d46ddb
bryndav <d.bryngelsson@gmail.com>
2016-10-11 20:15:26 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,28 +1,25 @@
-from PIL import Image
-import PIL.ExifTags
 import os
-import sort
+import sort.currentExif
 
 PATH = '/home/dbry/ownCloud/Foton/Mobilbilder'
 
-    
 for lib in os.listdir(PATH):
-        
+
     try:
         os.chdir(PATH + '/' + lib)
         newpath = PATH + '/' + lib
-            
+
         for file in os.listdir(newpath):
-                
+
             if file.endswith('.jpg') or file.endswith('.JPG'):
                 filepath = newpath + '/' + file
-            
-                currentIMG = sort.ImageInfo(filepath)
-                currentIMG.generateExif()
-                    
-                os.rename(file, currentIMG.exif['DateTime'][:-9].replace(':', '-')
-                            + '_' + currentIMG.exif['DateTime'][11:].replace(':', '-')
+
+                currentExif = sort.generateExif(filepath)
+
+                os.rename(file, currentExif['DateTime'][:-9].replace(':', '-')
+                            + '_' + currentExif['DateTime'][11:].replace(':', '-')
                             )
     except OSError: pass
+
 
             
