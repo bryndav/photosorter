a9d1e9e239f5b0a7a96aeaa1e753bb0cfa889618
bryndav <d.bryngelsson@gmail.com>
2016-10-11 13:30:25 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,38 +1,28 @@
 from PIL import Image
 import PIL.ExifTags
 import os
-import shutil
+import sort
 
 PATH = '/home/dbry/ownCloud/Foton/Mobilbilder'
 
-
-class ImageInfo:
-    """
-    A class for using the PIL lib with method for creating a dict with
-    picture header information.
-    """
-    def __init__(self, filepath):
-        self.img = Image.open(filepath)
-
-    def generateExif(self):
-        self.exif = {
-            PIL.ExifTags.TAGS[k]: v
-            for k, v in self.img._getexif().items()
-            if k in PIL.ExifTags.TAGS
-            }
-
-
-if __name__ == '__main__':
     
-    for entry in os.listdir(PATH):
+for lib in os.listdir(PATH):
         
-        if entry.endswith('.jpg') or entry.endswith('.JPG'):
-            filepath = PATH + '/' + entry
-            currentImg = ImageInfo(filepath)
-            currentImg.generateExif()
+    try:
+        os.chdir(PATH + '/' + lib)
+        newpath = PATH + '/' + lib
             
-            destination = PATH + '/' + currentImg.exif['DateTime'][:-12].replace(':', '-')
+        for file in os.listdir(newpath):
+                
+            if file.endswith('.jpg') or file.endswith('.JPG'):
+                filepath = newpath + '/' + file
             
-            if not os.path.isdir(destination): os.makedirs(destination)
-            shutil.move(filepath, destination)
+                currentIMG = sort.ImageInfo(filepath)
+                currentIMG.generateExif()
+                    
+                os.rename(file, currentIMG.exif['DateTime'][:-9].replace(':', '-')
+                            + '_' + currentIMG.exif['DateTime'][11:].replace(':', '-')
+                            )
+    except OSError: pass
+
             
